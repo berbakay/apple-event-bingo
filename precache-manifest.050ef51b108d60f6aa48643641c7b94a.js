44032f46acb76ee02496a6c1f91768db9283dac0
berbakay <rick.kay90@gmail.com>
2020-10-12 20:15:45 +0100

    Updates

---
+++ precache-manifest.050ef51b108d60f6aa48643641c7b94a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f9036d8c1e8604d1bad517cff00d39",
+    "url": "/apple-event-bingo/index.html"
+  },
+  {
+    "revision": "83bfc8300ce0166a7b0e",
+    "url": "/apple-event-bingo/static/css/main.ad8d70b6.chunk.css"
+  },
+  {
+    "revision": "5158a5c469776b8823e3",
+    "url": "/apple-event-bingo/static/js/2.7893bd5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apple-event-bingo/static/js/2.7893bd5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83bfc8300ce0166a7b0e",
+    "url": "/apple-event-bingo/static/js/main.759ba61b.chunk.js"
+  },
+  {
+    "revision": "7bf8a570fa9238a14fec",
+    "url": "/apple-event-bingo/static/js/runtime-main.e544afd1.js"
+  }
+]);
